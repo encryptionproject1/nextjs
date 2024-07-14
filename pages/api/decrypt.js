1842acad30d10ec69f0e0588aff109683ab35118
encryptionproject1 <farzserv@gmail.com>
2024-07-14 13:34:24 +0330

    Create decrypt.js

---
+++ pages/api/decrypt.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { key, encrypted_message } = req.body;
+    try {
+      const response = await axios.post('http://localhost/php/encrypt_decrypt.php', { key, encrypted_message });
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Decryption failed' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
