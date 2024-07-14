b94bda3649cef2711d0d7e4b7458ff343f0d1eb9
encryptionproject1 <farzserv@gmail.com>
2024-07-14 13:50:04 +0330

    Update decrypt.js

--- pages/api/decrypt.js
+++ pages/api/decrypt.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { key, encrypted_message } = req.body;
     try {
-      const response = await axios.post('http://localhost/php/encrypt_decrypt.php', { key, encrypted_message });
+      const response = await axios.post('https://dudeiptv.com/php/encrypt_decrypt.php', { key, encrypted_message });
       res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ error: 'Decryption failed' });
